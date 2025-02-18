cdacf4f5c0c71693c74247f4ca8687132403ae5a
KelveNunes <kelvenunes123@gmail.com>
2025-02-18 15:07:01 -0300

    lincando rotas de login com a rota principal

--- login/urls.py
+++ login/urls.py
@@ -3,6 +3,6 @@ from login.views import login,forgotPassword, newUser
 
 urlpatterns = [
     path('', login),
-    path('forgotPassword/', forgotPassword),
-    path('newUser/', newUser),
+    path('forgotPassword/', forgotPassword, name='forgot-password'),
+    path('newUser/', newUser, name='new-user'),
 ]
