e68ff57a07c8c8bc8377a1dacfd06ef7d190baa6
KelveNunes <kelvenunes123@gmail.com>
2025-02-18 15:34:49 -0300

    configurando arquivo base.html

--- login/urls.py
+++ login/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from login.views import login,forgotPassword, newUser
 
 urlpatterns = [
-    path('', login),
+    path('', login, name='login'),
     path('forgotPassword/', forgotPassword, name='forgot-password'),
     path('newUser/', newUser, name='new-user'),
 ]
