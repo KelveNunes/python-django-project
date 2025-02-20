b547b6dd9aec1a2928f2239479c2aa948890ee14
KelveNunes <kelvenunes123@gmail.com>
2025-02-20 11:43:23 -0300

    criando BD de login

--- login/models.py
+++ login/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class login(models.Model):
+    user = models.CharField(max_length=100, null=False, blank=False)
+
+    password = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return f"login [user={self.user}]"
